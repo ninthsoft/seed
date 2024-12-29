22a5ab8533bf16adce6555508ae7ebdb5e0c1241
DanJian <13260006106@163.com>
2024-12-29 21:30:49 +0800

    Cors middleware

---
+++ middleware/cors.go
@@ -0,0 +1,23 @@
+package middleware
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/hexthink/seed"
+)
+
+func CORS(ctx context.Context, w http.ResponseWriter, req *http.Request, next seed.MiddleWareQueue) bool {
+	w.Header().Add("Access-Control-Allow-Origin", "*")
+	w.Header().Add("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, OPTIONS")
+	w.Header().Add("Access-Control-Allow-Headers", "*")
+	w.Header().Add("Access-Control-Allow-Credentials", "true")
+
+	//preflight request
+	if req.Method == http.MethodOptions {
+		w.WriteHeader(http.StatusNoContent)
+		w.Write(nil)
+		return false
+	}
+	return next.Next(ctx, w, req)
+}
